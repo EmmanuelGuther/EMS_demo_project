79d39228cf610b757bed1d34e72e48f44b47a65e
emmanuelguther <emmanuelguther@gmail.com>
2022-03-11 11:25:32 +0100

    move files from app to core_presentation

---
+++ core_presentation/src/main/java/com/emmanuelguther/core_presentation/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.emmanuelguther.core_presentation.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val WetAsphalt = Color(0xFF34495E)
+val SunFlower = Color(0xFFF1C40F)
+val Nephritis = Color(0xFF27AE60)
+val Pumpkin = Color(0xFFD35400)
+val GreenSea = Color(0xFF16A085)
---
+++ core_presentation/src/main/java/com/emmanuelguther/core_presentation/ui/theme/Theme.kt
@@ -0,0 +1,57 @@
+package com.emmanuelguther.core_presentation.ui.theme
+
+import android.app.Activity
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.ViewCompat
+private val DarkColorScheme = darkColors(
+    primary = SunFlower,
+    secondary = Pumpkin,
+    secondaryVariant = GreenSea,
+    background = WetAsphalt
+)
+
+private val LightColorScheme = lightColors(
+    primary = SunFlower,
+    secondary = Pumpkin,
+    secondaryVariant = GreenSea,
+    background = WetAsphalt
+
+    /* Other default colors to override
+    background = Color(0xFFFFFBFE),
+    surface = Color(0xFFFFFBFE),
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onTertiary = Color.White,
+    onBackground = Color(0xFF1C1B1F),
+    onSurface = Color(0xFF1C1B1F),
+    */
+)
+
+@Composable
+fun CardGameTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            (view.context as Activity).window.statusBarColor = colorScheme.background.toArgb()
+            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+        }
+    }
+
+    MaterialTheme(
+        colors = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
