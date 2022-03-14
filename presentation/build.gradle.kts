2d62960176e09fd62fe6f66266cac6f6b34cde10
emmanuelguther <emmanuelguther@gmail.com>
2022-03-14 10:43:32 +0100

    enabled desugaring for works with date better

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     buildFeatures {
         compose = true
     }
-    
+
     defaultConfig {
         minSdk = AppConfig.minSdkVersion
         targetSdk = AppConfig.targetSdkVersion
@@ -20,6 +20,13 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = Versions.composeVersion
     }
+
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
     namespace = "com.emmanuelguther.features"
 }
 
@@ -29,12 +36,13 @@ dependencies {
     implementation(project(":domain"))
     dependenciesUI()
     dependenciesHilt()
+    coreLibraryDesugaring ("com.android.tools:desugar_jdk_libs:1.1.5")
 
-    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.4.1")
+   implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.1")
     implementation("androidx.navigation:navigation-compose:2.4.1")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
 
-    testImplementation ("junit:junit:4.13.2")
-    testImplementation ("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.5.2")
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.5.2")
 
 }
